3ec20c44602a8169c66d287eec6087f3183aa781
Heather Laude <hlaude@ufl.edu>
2015-04-16 18:42:50 -0400

    update file links for live site

---
+++ js/main2.js
@@ -0,0 +1,14 @@
+$(document).ready( function() {
+
+//backstretch
+    $.backstretch("img/brownbricks.jpg");
+
+//bxslider
+    $('.bxslider').bxSlider({
+        auto: true
+    });
+
+//mixitup
+    $('#mixitup').mixItUp();
+
+});
